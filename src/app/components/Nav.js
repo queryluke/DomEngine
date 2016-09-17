8315949ec54fb20f8788ae252f3aa5cd3fe9e5f8
Luke Aeschleman <lukeaeschleman@gmail.com>
2016-09-17 14:43:56 -0400

    mobile friendlier

--- src/app/components/Nav.js
+++ src/app/components/Nav.js
@@ -1,13 +1,15 @@
 class NavController {
+  /** @ngInject */
+  constructor($state) {
+    this.$state = $state;
+  }
+
   $onInit() {
-    this.showNav = false;
+    this.showMenu = false;
   }
 }
 
 export const Nav = {
   templateUrl: 'app/components/Nav.html',
-  controller: NavController,
-  bindings: {
-    onBuild: '&'
-  }
+  controller: NavController
 };
